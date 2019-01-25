97dd97564b03e7a51171cd0887e587b0d33ccc2b
Funkschy <felixschoeller@outlook.de>
2019-01-26 00:38:38 +0100

    fixed lava bug

--- Assets/Scripts/Lava.cs
+++ Assets/Scripts/Lava.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
     public GameObject[] players;
 
+    private IList<GameObject> _players;
+
+    private void Start()
+    {
+        _players = players.ToList();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        foreach (var player in players)
+        GameObject removed = null;
+        
+        foreach (var player in _players)
         {
             if (other.transform != player.transform) continue;
             var health = player.GetComponent<Health>();
+            _players.Remove(player);
             health.Kill();
+            removed = player;
+            break;
+        }
+        
+        if (removed != null)
+        {
+            _players.Remove(removed);
         }
     }
 }
