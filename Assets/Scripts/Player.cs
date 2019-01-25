2b97865af94b75c1a915317312b8d44d023e0911
Funkschy <felixschoeller@outlook.de>
2019-01-25 23:51:29 +0100

    destroy script and test lava

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int startHealth = 100;
+
+    private Health _health;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _health = gameObject.AddComponent<Health>();
+        _health.KillCallback = Die;
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
