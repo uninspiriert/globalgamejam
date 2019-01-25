ce4f8ca05bb620475b4ce5e86cd48f5555e80332
Jan Völker <jan-voelker@t-online.de>
2019-01-26 00:22:24 +0100

    pre select button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public UnityEngine.UI.Button button;
+    
+    // Starts the first scene
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Quits the game
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    // Select first Button for Controller compatabillity
+    private void OnEnable()
+    {
+        button.Select();
     }
 }
